71c1a7da55db85a5d5de3f0c7f7f0a347f24c7bd
Bhagavan <bhagavansprasad@gmail.com>
2025-01-08 10:15:59 +0530

    Update users.py

--- app/routers/users.py
+++ app/routers/users.py
@@ -4,8 +4,6 @@ from typing import List
 from app.dependencies import get_db
 from app.models import User
 from app.schemas import UserSchema
-from app.security import decode_access_token
-import logging
 
 router = APIRouter()
 
@@ -27,8 +25,6 @@ def list_users(
 
     # Extract the actual token
     token = token_parts[1]
-    decoded_data = decode_access_token(token)
-    logging.debug(f"decoded_data :{decoded_data}")
     
     # Fetch all active users
     users = db.query(User).filter(User.active_flag == 1).all()
