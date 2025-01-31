a573a9a5dd950ba0ced7c68384cdb6556a3ae76a
ikshwak <ikshwak@LAPTOP-HC0QPTVQ.localdomain>
2025-01-31 13:54:15 +0530

    adding the required changes

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel
 from pydantic import HttpUrl
 from typing import Optional
+from typing import List
+from datetime import datetime
+
+
 
 class UserBase(BaseModel):
     username: str
